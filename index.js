068f9dd3113a2174c9dd26ec2d39a468964b2ccb
Matthias Metzen <matthiasmetzen@web.de>
2021-05-20 17:13:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ const run = async () => {
     // Add all the Helm Secrets files
     secretsFiles.forEach((f) => args.push(`--values=secrets://${f}`));
     
-    additionalArgs && additionalArgs.forEach((f) => args.push(`${f}`));
+    additionalArgs.forEach((f) => args.push(`${f}`));
 
     // Add the individually added values
     await fsp.writeFile("/values.yml", values);
